05b6c827b480b48d3e7b293f485f01670e242564
olga-plotkina <olga.semenyuta@gmail.com>
2022-11-18 15:53:01 +0000

    fix localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,13 +10,6 @@ import { useReducer } from 'react';
 export function App() {
   const [filter, setFilter] = useState('');
 
-  const defaultValue = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-
   const changeFilter = event => {
     setFilter(event.currentTarget.value);
   };
@@ -53,11 +46,18 @@ export function App() {
         throw new Error(`Unsupported action type ${action.type}`);
     }
   }
+  function init(param) {
+    return (
+      JSON.parse(window.localStorage.getItem(param)) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  }
 
-  const [state, dispatch] = useReducer(
-    countReducer,
-    JSON.parse(window.localStorage.getItem('contacts')) ?? defaultValue
-  );
+  const [state, dispatch] = useReducer(countReducer, 'contacts', init);
 
   return (
     <Form>
